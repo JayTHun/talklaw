0edbb704a3318d82e98bf48161c87b63954cf4c7
JayTHun <deliverylee@naver.com>
2021-09-23 20:24:32 +0900

    test2

--- test.java
+++ test.java
@@ -1,1 +1,6 @@
-test    
+public class CallerApplication {
+    public static ApplicationContext applicationContext;
+    public static void main(String[] args) {
+        applicationContext = SpringApplication.run(nicecall.CallerApplication.class, args);
+    }
+}
