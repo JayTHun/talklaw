067dcab7ce9d39a50d6690f5d2accdca8e898944
JayTHun <88808353+JayTHun@users.noreply.github.com>
2021-09-23 20:32:13 +0900

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-public class CallerApplication {
-    public static ApplicationContext applicationContext;
-    public static void main(String[] args) {
-        applicationContext = SpringApplication.run(nicecall.CallerApplication.class, args);
-    }
-}
-
-
-
